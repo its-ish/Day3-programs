cc91f3f66cfffdd95aa403525b84e58864159b40
its-ish <148241548+its-ish@users.noreply.github.com>
2024-06-26 16:04:52 +0530

    Add files via upload

---
+++ splitstring.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String [] s=in.nextLine().split(" ");
+        for(int i=0;i<s.length;i++)
+        {
+            if(!s[i].equals("this"))
+            {
+                System.out.print(s[i]+" ");
+            }
+        }
+
+    }
+}
